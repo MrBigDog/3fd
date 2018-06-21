d14a75bf3beb0b9254e339f748d28d6fd20eaffc
Felipe Aburaya <felipe.aburaya@tomtom.com>
2018-06-21 11:54:48 +0200

    fixed compilation and improved binary search algorithms

--- 3FD/utils_algorithms.h
+++ 3FD/utils_algorithms.h
@@ -12,50 +12,6 @@ namespace _3fd
 {
 namespace utils
 {
-    template <typename IterType, typename KeyType, typename AnyCallableType>
-    class KeyGetter
-    {
-    private:
-
-        std::function<const KeyType &(typename std::iterator_traits<IterType>::reference) NOEXCEPT> m_wrappedCallable;
-
-    public:
-
-        KeyGetter(AnyCallableType callable)
-            : m_wrappedCallable(std::forward<AnyCallableType>(callable)) {}
-
-        KeyGetter(const KeyGetter &ob)
-            : m_wrappedCallable(ob.m_wrappedCallable) {}
-
-        constexpr KeyType &operator()(typename std::iterator_traits<IterType>::reference object) const NOEXCEPT
-        {
-            return m_wrappedCallable(object);
-        }
-    };
-
-    template <typename IterType, typename AnyCallableType>
-    class LessThanComparator
-    {
-    private:
-
-        std::function<bool (typename std::iterator_traits<IterType>::reference,
-            typename std::iterator_traits<IterType>::reference) NOEXCEPT> m_wrappedCallable;
-
-    public:
-
-        LessThanComparator(AnyCallableType callable)
-            : m_wrappedCallable(std::forward<AnyCallableType>(callable)) {}
-
-        LessThanComparator(const LessThanComparator &ob)
-            : m_wrappedCallable(ob.m_wrappedCallable) {}
-
-        constexpr bool operator()(typename std::iterator_traits<IterType>::reference left,
-                                  typename std::iterator_traits<IterType>::reference right) const NOEXCEPT
-        {
-            return m_wrappedCallable(left, right);
-        }
-    };
-
     /// <summary>
     /// Binary search in sub-range of vector containing map cases entries.
     /// </summary>
@@ -66,16 +22,17 @@ namespace utils
     /// In the end, this parameter keeps the end of the last sub-range this iterative
     /// algorithm has delved into.</param>
     /// <param name="searchKey">The key to search for.</param>
-    /// <returns>An iterator to the found entry. Naturally, it cannot refer to
-    /// the same position in the parameter 'end', unless there was no match.</returns>
+    /// <param name="getKey">A functor that retrieves the key for an object.</param>
+    /// <param name="lessThan">A functor that evaluates when a key value is less than another.</param>
+    /// <returns>An iterator to the found entry. If there was no match,
+    /// 'begin == end' in the position it was supposed to be found.</returns>
     template <typename IterType, typename SearchKeyType, typename GetKeyFnType, typename LessFnType>
     IterType BinarySearch(IterType &begin,
                           IterType &end,
                           SearchKeyType searchKey,
-                          KeyGetter<IterType, SearchKeyType, GetKeyFnType> getKey,
-                          LessThanComparator<IterType, LessFnType> lessThan) NOEXCEPT
+                          GetKeyFnType getKey,
+                          LessFnType lessThan) NOEXCEPT
     {
-        auto endOfRange = end;
         auto length = std::distance(begin, end);
 
         if (length > 7)
@@ -92,7 +49,7 @@ namespace utils
                     return middle;
             }
 
-            return begin = end = endOfRange;
+            return begin;
         }
 
         // Linear search when container is small:
@@ -102,26 +59,30 @@ namespace utils
             if (lessThan(getKey(*begin), searchKey))
                 ++begin;
             else if (lessThan(searchKey, getKey(*begin)))
-                break;
+                return end = begin;
             else
                 return begin;
         }
 
-        return begin = end = endOfRange;
+        return begin;
     }
 
-    template <typename IterType, typename SearchKeyType, typename GetKeyFnType, typename LessFnType>
-    IterType BinarySearch(IterType &&begin,
-                          IterType &&end,
-                          SearchKeyType searchKey,
-                          KeyGetter<IterType, SearchKeyType, GetKeyFnType> getKey,
-                          LessThanComparator<IterType, LessFnType> lessThan) NOEXCEPT
+    template <typename IterType1,
+              typename IterType2,
+              typename SearchKeyType,
+              typename GetKeyFnType,
+              typename LessFnType>
+    IterType1 BinarySearch(IterType1 &&begin,
+                           IterType2 &&end,
+                           SearchKeyType searchKey,
+                           GetKeyFnType getKey,
+                           LessFnType lessThan) NOEXCEPT
     {
         return BinarySearch(begin,
                             end,
                             searchKey,
-                            getKeyFunction,
-                            lessThanFunction);
+                            getKey,
+                            lessThan);
     }
 
     /// <summary>
@@ -132,53 +93,56 @@ namespace utils
     /// <param name="subRangeEnd">An iterator to one past the last position of
     /// the range to search, and receives the same for the found sub-range.</param>
     /// <param name="searchKey">The key to search.</param>
-    /// <returns>When a sub-range has been found, <c>true</c>, otherwise, <c>false</c>.</returns>
+    /// <param name="getKey">A functor that retrieves the key for an object.</param>
+    /// <param name="lessThan">A functor that evaluates when a key value is less than another.</param>
+    /// <returns>When a sub-range has been found, returns <c>true</c> and
+    /// 'subRangeBegin != subRangeEnd', otherwise, returns <c>false</c> and
+    /// 'subRangeBegin == subRangeEnd' in the position it was supposed to be found.</returns>
     template <typename IterType, typename SearchKeyType, typename GetKeyFnType, typename LessFnType>
     bool BinSearchSubRange(IterType &subRangeBegin,
                            IterType &subRangeEnd,
                            SearchKeyType searchKey,
-                           KeyGetter<IterType, SearchKeyType, GetKeyFnType> getKey,
-                           LessThanComparator<IterType, LessFnType> lessThan) NOEXCEPT
+                           GetKeyFnType getKey,
+                           LessFnType lessThan) NOEXCEPT
     {
-        struct {
-            IterType begin;
-            IterType end;
-        } firstMatchRightPart;
+        auto firstMatch = BinarySearch(subRangeBegin,
+                                       subRangeEnd,
+                                       searchKey,
+                                       getKey,
+                                       lessThan);
 
-        auto endOfRange = subRangeEnd;
-        auto end = subRangeEnd;
-        auto begin = subRangeBegin;
-        auto match = BinarySearch(begin, end, searchKey, getKey, lessThan);
+        // no match?
+        if (subRangeBegin == subRangeEnd)
+            return false;
 
-        // match? this is the first, so keep the partition at the right:
-        if (match != end)
-        {
-            firstMatchRightPart.begin = match; // match inclusive
-            firstMatchRightPart.end = end;
-        }
-        else
-        {
-            subRangeBegin = subRangeEnd = endOfRange;
-            return false; // no match found!
-        }
+        auto end = firstMatch;
 
-        do
+        do // iterate to find lower bound:
         {
-            subRangeBegin = end = match; // last match is the smallest entry found!
-            match = BinarySearch(begin, end, searchKey, getKey, lessThan); // continue to search in the left partition
-        } while (match != end);
+            // search another match in the left partition:
+            end = BinarySearch(subRangeBegin,
+                               end,
+                               searchKey,
+                               getKey,
+                               lessThan);
+
+        } while (subRangeBegin != end);
 
         /* Now go back to the partition at the right of the first
         match, and start looking for the end of the range: */
 
-        match = firstMatchRightPart.begin;
-        end = firstMatchRightPart.end;
+        auto begin = firstMatch;
 
-        do
+        do  // iterate to find upper bound:
         {
-            subRangeEnd = begin = match + 1; // last match is the greatest entry found!
-            match = BinarySearch(begin, end, searchKey, getKey, lessThan); // continue to search in the right partition
-        } while (match != end);
+            // search another match in the right partition:
+            begin = BinarySearch(begin + 1,
+                                 subRangeEnd,
+                                 searchKey,
+                                 getKey,
+                                 lessThan);
+
+        } while (begin != subRangeEnd);
 
         return true;
     }
